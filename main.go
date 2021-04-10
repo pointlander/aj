6dc7e53588b52affd7363e553389af55501d8327
Andrew Snodgrass <git@pointlander.info>
2021-04-10 13:50:04 -0600

    first commit

---
+++ main.go
@@ -0,0 +1,165 @@
+// Copyright 2021 The Neural Cluster Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"math"
+	"math/rand"
+	"time"
+
+	"gonum.org/v1/plot"
+	"gonum.org/v1/plot/plotter"
+	"gonum.org/v1/plot/vg"
+	"gonum.org/v1/plot/vg/draw"
+
+	"github.com/pointlander/datum/mnist"
+	"github.com/pointlander/gradient/tf32"
+)
+
+const (
+	// Width is the width of the neural network
+	Width = mnist.Width * mnist.Height
+	// BatchSize is the size of a batch
+	BatchSize = 100
+)
+
+func main() {
+	rand.Seed(1)
+
+	flag.Parse()
+
+	datum, err := mnist.Load()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("images", len(datum.Train.Images))
+
+	set := tf32.NewSet()
+	set.Add("aw1", Width, 4*Width)
+	set.Add("ab1", 4*Width)
+	set.Add("aw2", 4*Width, 10)
+	set.Add("ab2", 10)
+
+	for i := range set.Weights {
+		w := set.Weights[i]
+		if w.S[1] == 1 {
+			for i := 0; i < cap(w.X); i++ {
+				w.X = append(w.X, 0)
+			}
+		} else {
+			factor := float32(math.Sqrt(2 / float64(w.S[0])))
+			for i := 0; i < cap(w.X); i++ {
+				w.X = append(w.X, float32(rand.NormFloat64())*factor)
+			}
+		}
+	}
+
+	deltas := make([][]float32, 0, 8)
+	for _, p := range set.Weights {
+		deltas = append(deltas, make([]float32, len(p.X)))
+	}
+
+	image := tf32.NewV(Width, BatchSize)
+	image.X = image.X[:cap(image.X)]
+	label := tf32.NewV(10, BatchSize)
+	label.X = label.X[:cap(label.X)]
+
+	indexes := make([]int, len(datum.Train.Images))
+	for i := range indexes {
+		indexes[i] = i
+	}
+
+	l1 := tf32.Sigmoid(tf32.Add(tf32.Mul(set.Get("aw1"), image.Meta()), set.Get("ab1")))
+	l2 := tf32.Sigmoid(tf32.Add(tf32.Mul(set.Get("aw2"), l1), set.Get("ab2")))
+	cost := tf32.Avg(tf32.Quadratic(label.Meta(), l2))
+
+	iterations := 60
+	points := make(plotter.XYs, 0, iterations)
+	for i := 0; i < iterations; i++ {
+		for i := range indexes {
+			j := i + rand.Intn(len(indexes)-i)
+			indexes[i], indexes[j] = indexes[j], indexes[i]
+		}
+
+		total := float32(0.0)
+		start := time.Now()
+		for i := 0; i < len(indexes); i += BatchSize {
+			weights := set.ByName["aw1"]
+			weights.Drop = .5
+
+			weights = set.ByName["ab1"]
+			weights.Drop = .5
+
+			set.Zero()
+			image.Zero()
+			label.Zero()
+			index := 0
+			for j := 0; j < BatchSize; j++ {
+				for _, value := range datum.Train.Images[indexes[i+j]] {
+					image.X[index] = float32(value) / 255
+					index++
+				}
+			}
+			for j := range label.X {
+				label.X[j] = 0
+			}
+			index = 0
+			for j := 0; j < BatchSize; j++ {
+				label.X[index+int(datum.Train.Labels[indexes[i+j]])] = 1
+				index += 10
+			}
+
+			total += tf32.Gradient(cost).X[0]
+			norm := float32(0)
+			for _, p := range set.Weights {
+				for _, d := range p.D {
+					norm += d * d
+				}
+			}
+			norm = float32(math.Sqrt(float64(norm)))
+			scaling := float32(1)
+			if norm > 1 {
+				scaling = 1 / norm
+			}
+
+			alpha, eta := float32(.3), float32(.3)
+			for k, p := range set.Weights {
+				for l, d := range p.D {
+					deltas[k][l] = alpha*deltas[k][l] - eta*d*scaling
+					p.X[l] += deltas[k][l]
+				}
+			}
+
+			if i%1000 == 0 {
+				fmt.Print(".")
+			}
+		}
+
+		fmt.Println("")
+		points = append(points, plotter.XY{X: float64(i), Y: float64(total)})
+		fmt.Println(i, total, time.Now().Sub(start))
+	}
+
+	p := plot.New()
+
+	p.Title.Text = "epochs vs cost"
+	p.X.Label.Text = "epochs"
+	p.Y.Label.Text = "cost"
+
+	scatter, err := plotter.NewScatter(points)
+	if err != nil {
+		panic(err)
+	}
+	scatter.GlyphStyle.Radius = vg.Length(1)
+	scatter.GlyphStyle.Shape = draw.CircleGlyph{}
+	p.Add(scatter)
+
+	err = p.Save(8*vg.Inch, 8*vg.Inch, "epochs.png")
+	if err != nil {
+		panic(err)
+	}
+}
